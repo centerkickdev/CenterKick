e61294e87b0f8c1d71736d44bb15ee1306b0df6d
Joseph Unomieta <lionelunomieta@gmail.com>
2026-07-12 19:46:41 +0100

    added both YouTube thumbnail domains to the RPs

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async headers() {
@@ -55,4 +67,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-// Cache-bust reboot trigger v3
+// Cache-bust reboot trigger v4
